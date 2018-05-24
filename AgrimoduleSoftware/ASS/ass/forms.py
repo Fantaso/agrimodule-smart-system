2e49449e3594ea4a1e6aa6ddab5cebeeef4df5b7
Fantaso <fantaso.code@gmail.com>
2018-05-24 22:10:54 +0200

    refactor agrimodulesystem db: 1. change the var pass to templates changed from the db. 2. erase many spaces to reduce lines of code. more comppressed better readable

--- AgrimoduleSoftware/ASS/ass/forms.py
+++ AgrimoduleSoftware/ASS/ass/forms.py
@@ -9,78 +9,41 @@ from flask_uploads import IMAGES
 # WEBSITE FORMS
 # Templates
 class EmailForm(FlaskForm):
-	email 	= StringField('Email',
-    						validators=[
-    								DataRequired(),
-    								Length(min=5, max=30, message=None),
-    								Email()])
+	email 	= StringField('Email', validators=[DataRequired(), Length(min=5, max=30, message=None), Email()])
 
 class EmailAndTextForm(EmailForm):
-	msg     = TextAreaField('Message',
-                            validators=[
-                                    DataRequired(),
-                                    Length(min=-1, max=1000, message='Maximum characters: 1000')])
+	msg     = TextAreaField('Message', validators=[DataRequired(), Length(min=-1, max=1000, message='Maximum characters: 1000')])
 
 class ContactUsForm(EmailAndTextForm):
-    name 	= StringField(label='Fullname',
-    						validators=[
-    								DataRequired(),
-    								Length(min=3, max=30, message=None)])
-    phone 	= StringField('Phone',
-    						validators=[
-    								DataRequired(),
-    								Length(min=7, max=30, message=None)])
+    name 	= StringField(label='Fullname', validators=[DataRequired(), Length(min=3, max=30, message=None)])
+    phone 	= StringField('Phone', validators=[DataRequired(), Length(min=7, max=30, message=None)])
 #Extended form for register flask-security
 class RegisterFormExt(RegisterForm, ConfirmRegisterForm):
-    name        = StringField(label='Name',
-                            validators=[
-                                    DataRequired(),
-                                    Length(min=2, max=30, message='''Your name needs
-                                                                        at least 2 characters.''')])
-    last_name   = StringField(label='Last name',
-                            validators=[
-                                    DataRequired(),
-                                    Length(min=2, max=30, message='''Your last name needs
-                                                                        at least 2 characters.''')])
+    name        = StringField(label='Name', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
+    last_name   = StringField(label='Last name', validators=[DataRequired(), Length(min=2, max=30, message='''Your last name needs at least 2 characters.''')])
     birthday   = DateField(label='Birthday', format='%Y-%m-%d')
-    mobile   = StringField('Mobile',
-                            validators=[
-                                    DataRequired(),
-                                    Length(min=7, max=30, message=None)])
+    mobile   = StringField('Mobile', validators=[DataRequired(), Length(min=7, max=30, message=None)])
 
 # SET UP FARM FORMS
 
 # SET UP AGRIMODULE FORMS
-
 class CultivationProcessForm(FlaskForm):
-    cultivation_process     = SelectField(label='Cultivation Process',
-                                    validators=[DataRequired()],
-                                    choices=[('Organic','Organic'),('Chemical','Chemical')])
+    cultivation_process     = SelectField(label='Cultivation Process', validators=[DataRequired()], choices=[('Organic','Organic'),('Chemical','Chemical')])
 
 class CultivationTypeForm(FlaskForm):
-    cultivation_type        = SelectField(label='Cultivation Type',
-                                                validators=[DataRequired()],
-                                                choices=[('mono','Mono'), ('mix','Mix'), ('multi','Multi')])
+    cultivation_type        = SelectField(label='Cultivation Type', validators=[DataRequired()], choices=[('mono','Mono'), ('mix','Mix'), ('multi','Multi')])
+
 class CultivationStateForm(FlaskForm):
-    cultivation_state       = SelectField(label='Cultivation State',
-                                                validators=[DataRequired()],
-                                                choices=[('new','New'),('Already Growing','Already Growing')])
+    cultivation_state       = SelectField(label='Cultivation State', validators=[DataRequired()], choices=[('new','New'),('Already Growing','Already Growing')])
 
 class CultivationStartDateForm(FlaskForm):
-    cultivation_start_date  = DateField(label='Cultivation Start Date', format='%Y-%m-%d',
-                                                validators=[DataRequired()])
+    cultivation_start_date  = DateField(label='Cultivation Start Date', format='%Y-%m-%d', validators=[DataRequired()])
 
 class CultivationCropForm(FlaskForm):
-    cultivation_crop        = SelectField(label='Cultivation Crop',
-                                                validators=[DataRequired()],
-                                                choices=[('plum','Plum'),('romaine','Romaine'),('arugula','Arugula')],
-                                                option_widget=None)
+    cultivation_crop        = SelectField(label='Cultivation Crop', validators=[DataRequired()], choices=[('plum','Plum'),('romaine','Romaine'),('arugula','Arugula')], option_widget=None)
 
 class CultivationAreaForm(FlaskForm):
-    cultivation_area       = FloatField(label='Cultivation Area',
-                                     validators=[DataRequired(),
-                                                 NumberRange(min=5, max=5000, message='Area between 5 and 5000 m2')]) 
-
+    cultivation_area       = FloatField(label='Cultivation Area', validators=[DataRequired(), NumberRange(min=5, max=5000, message='Area between 5 and 5000 m2')]) 
 
 class FarmForm(FlaskForm):
     farm_name                   = StringField('Farm name', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
@@ -90,25 +53,13 @@ class FarmForm(FlaskForm):
 
 
 class FieldForm(FlaskForm):
-    field_name                    = StringField(label='Field name',
-                                                validators=[
-                                                        DataRequired(),
-                                                        Length(min=2, max=30, message='''Your name needs
-                                                                                         at least 2 characters.''')])
-    
-    field_cultivation_area       = FloatField(label='Field Cultivation Area',
-                                                validators=[DataRequired(), NumberRange(min=1, max=5000, message='Cultivation area should be maximum as big as your farm')],render_kw={"placeholder":"500.50"})
-
+    field_name                    = StringField(label='Field name', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
+    field_cultivation_area       = FloatField(label='Field Cultivation Area', validators=[DataRequired(), NumberRange(min=1, max=5000, message='Cultivation area should be maximum as big as your farm')],render_kw={"placeholder":"500.50"})
     field_cultivation_crop        = SelectField(label='Cultivation Crop', validators=[DataRequired()], coerce = int)
+    field_cultivation_start_date  = DateField(label='Cultivation Start Date', format='%Y-%m-%d', validators=[DataRequired()])
+    field_cultivation_state       = SelectField(label='Cultivation State', validators=[DataRequired()], choices=[('new','New'),('already growing','Already Growing')])
+    field_cultivation_type        = SelectField(label='Cultivation Type', validators=[DataRequired()], choices=[('mono','Mono'), ('mix','Mix'), ('multi','Multi')])
 
-    field_cultivation_start_date  = DateField(label='Cultivation Start Date', format='%Y-%m-%d',
-                                                validators=[DataRequired()])
-    field_cultivation_state       = SelectField(label='Cultivation State',
-                                                validators=[DataRequired()],
-                                                choices=[('new','New'),('already growing','Already Growing')])
-    field_cultivation_type        = SelectField(label='Cultivation Type',
-                                                validators=[DataRequired()],
-                                                choices=[('mono','Mono'), ('mix','Mix'), ('multi','Multi')])
 class PreNewCropForm:
     def __init__(self, field_cultivation_area, field_cultivation_start_date, field_cultivation_state, field_cultivation_type):
         self.field_cultivation_area          = field_cultivation_area
@@ -175,13 +126,13 @@ class UserProfileForm(FlaskForm):
 
 # FARM SETUP SYSTEM
 class FarmInfoForm(FlaskForm):
-    farm_name                    = StringField('Farm name',      validators=[DataRequired(), Length(min=2, max=30, message='Your farm name needs to be at least 2 characters long.')])
+    farm_name                    = StringField('Farm name', validators=[DataRequired(), Length(min=2, max=30, message='Your farm name needs to be at least 2 characters long.')])
     farm_location                = StringField('Farm location (city)',   validators=[DataRequired(), Length(max=30, message='Type the city name and it needs to be max 30 characters long.')])
     farm_area                    = FloatField('Farm area (meter square)', validators=[DataRequired(), NumberRange(min=5, max=5000, message='Area between 5 and 5000 m2')])
     farm_cultivation_process     = SelectField('Farm Cultivation Process', validators=[DataRequired()], choices=[('Organic','Organic'),('Chemical','Chemical')])
 
 class AddAgrisysForm(FlaskForm):
-    agsys_identifier             = StringField('Agrimodule system code',      validators=[DataRequired(), Length(min=2, max=30, message='Your agrimodule system identifier is in the back of your agrimodule.')])
+    agrimodule_identifier        = StringField('Agrimodule code', validators=[DataRequired(), Length(min=2, max=30, message='Your agrimodule system identifier is in the back of your agrimodule.')])
 
 class InstallAgrisysForm(FlaskForm):
     agm_lat                 = FloatField('Agrimodule latitude location',       validators=[DataRequired(), NumberRange(min=-90, max=90, message='write the lat coordinates')])
@@ -205,32 +156,17 @@ class ContactUsForm:
         self.phone = phone 
         self.msg = msg
 
-
 # field enclosures - can
 class PhoneForm(Form):
     country_code = IntegerField(label='Country Code:') 
     area_code = IntegerField(label='Area Code:') 
     number = IntegerField(label='Number:') 
 
-
 class ContactUsFormEG(FlaskForm):
-    name    = StringField(label='Fullname',
-                            validators=[
-                                    DataRequired(),
-                                    Length(min=3, max=30, message=None)])
-    email   = StringField('Email',
-                            validators=[
-                                    DataRequired(),
-                                    Length(min=5, max=30, message=None),
-                                    Email()])
-    phone   = StringField('Phone',
-                            validators=[
-                                    DataRequired(),
-                                    Length(min=7, max=30, message=None)])
-    msg     = TextAreaField ('Message',
-                            validators=[
-                                    DataRequired(),
-                                    Length(min=-1, max=1000, message='Maximum characters: 1000')])
+    name    = StringField(label='Fullname', validators=[DataRequired(), Length(min=3, max=30, message=None)])
+    email   = StringField('Email', validators=[DataRequired(), Length(min=5, max=30, message=None), Email()])
+    phone   = StringField('Phone', validators=[DataRequired(), Length(min=7, max=30, message=None)])
+    msg     = TextAreaField ('Message', validators=[DataRequired(), Length(min=-1, max=1000, message='Maximum characters: 1000')])
     # Inherits from PhoneForm the fields that are there
     home_phone  = FormField(PhoneForm)
     handy_phone = FormField(PhoneForm)
@@ -239,10 +175,8 @@ class ContactUsFormEG(FlaskForm):
 
 # ContactUsFormExtended inherits all fields from ContactUsForm
 class ContactUsFormExtended(ContactUsFormEG):
-    city    = StringField(label='City', validators=[
-                                    DataRequired()])
-    country = StringField(label='Country',
-                            validators=[DataRequired()])
+    city    = StringField(label='City', validators=[DataRequired()])
+    country = StringField(label='Country', validators=[DataRequired()])
 
 class ContactUsFormExtendedOne(ContactUsFormEG):
     start_date = DateField(label='Start Date', format='%Y-%m-%d')
