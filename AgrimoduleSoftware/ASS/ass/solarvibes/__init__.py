080a151a7bb0efb2ae5bc9d53a0a9819b08fe973
fantaso <fantaso.code@gmail.com>
2018-06-12 18:59:55 +0200

    first package structruing: 1. created new project folder called "solarvibes" 2. move to solarvibes folder: templates and staticv folder, .db, forms.py and cfg files 3. created the init,.py file 4. moved the configuration code to init.py 5. created the views and models.py to structurize app 6. ass.py renamed to run.py, which contains only the manager or run.app 7. changed the path of db URI to new path 8. added all the imports for the new connections between views, models, forms, _init__ and run.py

---
+++ AgrimoduleSoftware/ASS/ass/solarvibes/__init__.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from flask_mail import Mail
+
+from flask_uploads import IMAGES, UploadSet, configure_uploads
+
+from solarvibes.forms import RegisterFormExt
+
+
+
+#############################
+#############################
+# APP
+#############################
+#############################
+
+app = Flask(__name__)                               # creates the flask app
+photos =  UploadSet('photos', IMAGES)               # Flask-Uploads
+app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'       # Flask-Uploads
+app.config.from_pyfile('cfg.cfg')                   # imports app configuration from cfg.cfg
+app.config.from_pyfile('cfg2.cfg')                   # imports app configuration from cfg.cfg
+
+configure_uploads(app, photos)
+
+db = SQLAlchemy(app)                                # create database connection object
+migrate = Migrate(app, db)                          # creates a migration object for the app db migrations]\
+mail = Mail(app)
+
+# TO MANAGE THE MIGRATIONS WITH FLASK-SCRIPT WITH PYTHON EXTERNAL SCRIPTS > goes together to migrations for migraing db
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+# HERE STARTS THE MODELS
+from solarvibes.models import NewsletterTable, AgrimoduleFBTable, PlatformFBTable, WorkWithUsTable, ContactUsTable, roles_users, Role, User, Farm, Field, DailyFieldInput, Crop, Pump, Agrimodule, Agrisensor, Measurement, Agripump
+# HERE ENDS THE MODELS
+
+#############################
+#############################
+# Setup Flask-Security
+#############################
+#############################
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore, register_form=RegisterFormExt, confirm_register_form=RegisterFormExt)
+
+# HERE STARTS THE ROUTES
+from solarvibes import views
+# HERE ENDS THE ROUTES
