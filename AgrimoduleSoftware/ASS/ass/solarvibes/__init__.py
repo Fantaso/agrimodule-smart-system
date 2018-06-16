086a0ed239d9903a4fa99d8227ac388d6c996371
fantaso <fantaso.code@gmail.com>
2018-06-16 21:04:47 +0200

    blueprints: register new blueprints, login check agrimodule agripump agrisensor split the models import as per organized better

--- AgrimoduleSoftware/ASS/ass/solarvibes/__init__.py
+++ AgrimoduleSoftware/ASS/ass/solarvibes/__init__.py
@@ -1,29 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security, SQLAlchemyUserDatastore
-
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-
 from flask_mail import Mail
-
 from flask_uploads import IMAGES, UploadSet, configure_uploads
-
 from solarvibes.forms import RegisterFormExt
+from solarvibes.config import Config
 
 
 
-#############################
-#############################
-# APP
-#############################
-#############################
+
 
 app = Flask(__name__)                               # creates the flask app
 photos =  UploadSet('photos', IMAGES)               # Flask-Uploads
 app.config['UPLOADED_PHOTOS_DEST'] = 'static/uploads'       # Flask-Uploads
-app.config.from_pyfile('cfg.cfg')                   # imports app configuration from cfg.cfg
-app.config.from_pyfile('cfg2.cfg')                   # imports app configuration from cfg.cfg
+app.config.from_object(Config)                   # imports app configuration from config.py
 
 configure_uploads(app, photos)
 
@@ -35,20 +27,30 @@ mail = Mail(app)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
-# HERE STARTS THE MODELS
-from solarvibes.models import roles_users, Role, User, Farm, Field, DailyFieldInput, Crop, Pump, Agrimodule, Agrisensor, Measurement, Agripump
-# HERE ENDS THE MODELS
+#############################
+# Begin Import Models
+#############################
+from solarvibes.models import roles_users, Role, User
+from solarvibes.models import Farm, Field, DailyFieldInput, Crop
+from solarvibes.models import Agrimodule, Agrisensor, Measurement, Agripump, Pump
+#############################
+# End Import Models
+#############################
+
 
 #############################
+# Begin Setup Flask-Security
 #############################
-# Setup Flask-Security
-#############################
-#############################
-
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore, register_form=RegisterFormExt, confirm_register_form=RegisterFormExt)
+#############################
+# End Setup Flask-Security
+#############################
 
-# HERE STARTS THE ROUTES
+
+#############################
+# Begin Import Views
+#############################
 from solarvibes import views
 from solarvibes.site.views import site
 from solarvibes.users.views import users
@@ -58,13 +60,23 @@ from solarvibes.settings.views import settings
 from solarvibes.farm_settings.views import farm_settings
 from solarvibes.agrimodule_settings.views import agrimodule_settings
 from solarvibes.pump_settings.views import pump_settings
+from solarvibes.agrimodule.views import agrimodule
+from solarvibes.agripump.views import agripump
+from solarvibes.crop_status.views import crop_status
+from solarvibes.login_check.views import login_check
 
 app.register_blueprint(site, url_prefix='/site')
 app.register_blueprint(users, url_prefix='/users')
 app.register_blueprint(welcome, url_prefix='/welcome')
 app.register_blueprint(main, url_prefix='/main')
 app.register_blueprint(settings, url_prefix='/settings')
-app.register_blueprint(farm_settings, url_prefix='/farm_settings')
-app.register_blueprint(agrimodule_settings, url_prefix='/agrimodule_settings')
-app.register_blueprint(pump_settings, url_prefix='/pump_settings')
-# HERE ENDS THE ROUTES
+app.register_blueprint(farm_settings, url_prefix='/farm-settings')
+app.register_blueprint(agrimodule_settings, url_prefix='/agrimodule-settings')
+app.register_blueprint(pump_settings, url_prefix='/pump-settings')
+app.register_blueprint(agrimodule, url_prefix='/agrimodule')
+app.register_blueprint(agripump, url_prefix='/agripump')
+app.register_blueprint(crop_status, url_prefix='/crop-status')
+app.register_blueprint(login_check, url_prefix='/login-check')
+#############################
+# End Import Views
+#############################
