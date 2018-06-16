0a787655d44a08a926ddb053c69758afabe4104d
fantaso <fantaso.code@gmail.com>
2018-06-17 00:03:43 +0200

    postgresql: 1. removed the text lenght in the db class since the postgeresql didnot accpted the l;emnght of a text area

--- AgrimoduleSoftware/ASS/ass/solarvibes/site/models.py
+++ AgrimoduleSoftware/ASS/ass/solarvibes/site/models.py
@@ -17,21 +17,21 @@ class AgrimoduleFBTable(db.Model):
     __tablename__ = 'agrimodulefbtable'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30))
-    msg = db.Column(db.Text(length=1000))
+    msg = db.Column(db.Text())
     _time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
 class PlatformFBTable(db.Model):
     __tablename__ = 'platformfbtable'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30))
-    msg = db.Column(db.Text(length=1000))
+    msg = db.Column(db.Text())
     _time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
 class WorkWithUsTable(db.Model):
     __tablename__ = 'workwithustable'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(30))
-    msg = db.Column(db.Text(length=1000))
+    msg = db.Column(db.Text())
     _time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
 
 class ContactUsTable(db.Model):
@@ -40,5 +40,5 @@ class ContactUsTable(db.Model):
     name = db.Column(db.String(30))
     email = db.Column(db.String(30))
     phone = db.Column(db.String(30))
-    msg = db.Column(db.Text(length=1000))
+    msg = db.Column(db.Text())
     _time_created = db.Column(db.DateTime(timezone=True), server_default=func.now())
