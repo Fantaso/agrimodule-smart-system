769979dd5f309540386137791002940580cebf5b
fantaso <fantaso.code@gmail.com>
2018-06-18 19:16:56 +0200

    agrimodule blueprint: 1. added the bulma template for the agrimodule and changed the other templates to others names like sho2 or 3 to keep as backup. 2. modified the views to accept any agrimodule with id or any agrisensor connecto to that agrimodule with its id

--- AgrimoduleSoftware/ASS/ass/solarvibes/agrimodule/views.py
+++ AgrimoduleSoftware/ASS/ass/solarvibes/agrimodule/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, redirect, url_for
-from solarvibes.models import User, Agrimodule
+from flask import Blueprint, render_template, redirect, url_for, flash
+from solarvibes.models import User, Agrimodule, Measurement, Agrisensor
 from flask_login import current_user
 from flask_security import login_required
 
@@ -11,39 +11,87 @@ agrimodule = Blueprint(
 
 
 ##################
-# USER AGRIMODULE
+# USER SHOW AGRIMODULE
 ##################
 @agrimodule.route('/', methods=['GET'])
+@agrimodule.route('/<agrimodule_id>', methods=['GET'])
 @login_required
-def show():
-    user = User.query.filter_by(id = current_user.get_id()).first()
+def show_agrimodule(agrimodule_id = None):
+
+    if Agrimodule.query.filter_by(id=agrimodule_id).first() == None:
+        flash('That agrimodule do NOT exist')
+        return redirect(url_for(main.index))
+
+    if agrimodule_id == None:
+        flash('page not allowed')
+        return redirect(url_for(main.index))
+
+    user = current_user
+    agrimodule = current_user.agrimodules.filter_by(id = agrimodule_id).first()
     farm = user.farms.first()
     field = farm.fields.first()
+    system_name = agrimodule.name
     crop = field.crops.first()
-    print (crop)
-    print (crop._soil_ph_min)
-    print (crop._soil_ph_max)
-    print (crop._soil_temp_min)
-    print (crop._soil_temp_max)
-    print (crop._soil_humi_min)
-    print (crop._soil_humi_max)
-    print (crop._soil_nutrient_min)
-    print (crop._soil_nutrient_max)
-    print (crop._air_temp_min)
-    print (crop._air_temp_max)
-    print (crop._air_humi_min)
-    print (crop._air_humi_max)
+    measurement = agrimodule.measurements.order_by(Measurement.timestamp.desc()).first()
 
-    ag_measure = Agrimodule.query.first()
-    print (ag_measure)
-    print (ag_measure.soil_ph)
-    print (ag_measure.soil_nutrient)
-    print (ag_measure.soil_temp)
-    print (ag_measure.soil_humi)
-    print (ag_measure.air_temp)
-    print (ag_measure.air_humi)
-    print (ag_measure.air_pres)
-    print (ag_measure.solar_radiation)
-    print (ag_measure.batt_status)
+    return render_template('agrimodule/show.html', sensor_id = agrimodule_id, sensor = agrimodule, measurement = measurement, crop = crop, farm = farm, field = field, sensortype = 'Agrimodule', system_name = system_name)
 
-    return render_template('agrimodule/show.html', ag_measure = ag_measure, crop = crop)
+##################
+# USER SHOW AGRISENSOR
+##################
+@agrimodule.route('/<agrimodule_id>/agrisensor', methods=['GET'])
+@agrimodule.route('/<agrimodule_id>/agrisensor/<agrisensor_id>', methods=['GET'])
+@login_required
+def show_agrisensor(agrimodule_id = None, agrisensor_id = None):
+
+    if Agrimodule.query.filter_by(id=agrimodule_id).first() == None:
+        flash('That agrimodule do NOT exist')
+        return redirect(url_for(main.index))
+    if Agrisensor.query.filter_by(id=agrisensor_id).first() == None:
+        flash('That agrisensor do NOT exist')
+        return redirect(url_for(main.index))
+
+    if agrimodule_id == None:
+        flash('page not allowed')
+        return redirect(url_for(main.index))
+    if agrisensor_id == None:
+        flash('pag not allowed')
+        return redirect(url_for(main.index))
+
+    user = current_user
+    agrimodule = current_user.agrimodules.filter_by(id = agrimodule_id).first()
+    agrisensor = agrimodule.agrisensors.filter_by(id = agrisensor_id).first()
+    farm = user.farms.first()
+    field = farm.fields.first()
+    system_name = agrimodule.name
+    crop = field.crops.first()
+    measurement = agrimodule.measurements.order_by(Measurement.timestamp.desc()).first()
+
+    return render_template('agrimodule/show.html', sensor_id = agrisensor_id, sensor = agrisensor, measurement = measurement, crop = crop, farm = farm, field = field, sensortype = 'Agrisensor', system_name = system_name)
+
+
+# print (crop._soil_ph_min)
+# print (crop._soil_ph_max)
+# print (crop._soil_temp_min)
+# print (crop._soil_temp_max)
+# print (crop._soil_humi_min)
+# print (crop._soil_humi_max)
+# print (crop._soil_nutrient_min)
+# print (crop._soil_nutrient_max)
+# print (crop._air_temp_min)
+# print (crop._air_temp_max)
+# print (crop._air_humi_min)
+# print (crop._air_humi_max)
+
+# print(ag_measurement.timestamp)
+# print(ag_measurement.soil_ph)
+# print(ag_measurement.soil_nutrient)
+# print(ag_measurement.soil_temp)
+# print(ag_measurement.soil_humi)
+# print(ag_measurement.air_temp)
+# print(ag_measurement.air_humi)
+# print(ag_measurement.air_pres)
+# print(ag_measurement.solar_radiation)
+# print(ag_measurement.batt_status)
+# print(ag_measurement.lat)
+# print(ag_measurement.lon)
