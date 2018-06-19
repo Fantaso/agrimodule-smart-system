8ec971face2e99226d5552a6fe86faa96755b5f0
fantaso <fantaso.code@gmail.com>
2018-06-19 19:02:55 +0200

    crop_status blueprint: 1. created the new route. 2. added new validation and pass through more variables to be rendered. 3. created the new template with new style from bulma

--- AgrimoduleSoftware/ASS/ass/solarvibes/crop_status/views.py
+++ AgrimoduleSoftware/ASS/ass/solarvibes/crop_status/views.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for
-from solarvibes.models import User
+from flask import Blueprint, render_template, redirect, url_for, flash
+from solarvibes.models import User, Field, Farm, Agrimodule
 from flask_login import current_user
 from flask_security import login_required
+from datetime import datetime, timezone
 
 crop_status = Blueprint(
     'crop_status',
@@ -14,14 +15,45 @@ crop_status = Blueprint(
 # USER CROP STATUS
 ##################
 @crop_status.route('/', methods=['GET'])
+@crop_status.route('/<field_id>', methods=['GET'])
 @login_required
-def show():
+def show(field_id = None):
 
-    user = User.query.filter_by(id = current_user.get_id()).first()
-    farm = current_user.farms.first()
-    field = farm.fields.first()
+    # validation
+    if field_id == None:
+        flash('page not allowed')
+        return redirect(url_for('main.index'))
+
+    if Field.query.filter_by(id=field_id).first() == None:
+        flash('That agrimodule do NOT exist')
+        return redirect(url_for('main.index'))
+
+    def cm2_to_m2(cm2):
+            return cm2 / 10000
+
+    def m2_to_cm2(m2):
+            return m2 * 10000
+
+    # db objects
+    user = current_user
+    field = Field. query.filter_by(id = field_id).first()
+    agrimodule = Agrimodule.query.filter_by(field_id = field.id).first()
+    farm = Farm.query.filter_by(id = field.farm_id).first()
+    # TODO: here is for only 1 crop in the field. but when mix cultivation or multi. need to reflect more than 1 crop
     crop = field.crops.first()
 
-    cycle_days_so_far = ( datetime.now() - field.field_cultivation_start_date ).days
+    cycle_days_so_far = ( datetime.now(timezone.utc) - field.field_cultivation_start_date ).days
+    today = datetime.now(timezone.utc)
 
-    return render_template('crop_status/show.html', crop = crop, field = field, cycle_days_so_far = cycle_days_so_far)
+    time_bar_percentage = (cycle_days_so_far * 100) / (crop._dtm + crop._dtg)
+    yield_bar_percentage = (field.field_current_yield * 100) / (field.field_projected_yield)
+
+    return render_template('crop_status/show.html',
+                                crop = crop,
+                                farm = farm,
+                                field = field,
+                                today = today,
+                                cycle_days_so_far = cycle_days_so_far,
+                                time_bar_percentage = time_bar_percentage,
+                                yield_bar_percentage = yield_bar_percentage,
+                                system_name = agrimodule.name)
