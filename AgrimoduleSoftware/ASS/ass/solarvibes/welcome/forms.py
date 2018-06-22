93ebed1fb4b7cf626b16d86da3806f44bcffd8e8
fantaso <fantaso.code@gmail.com>
2018-06-22 07:16:42 +0200

    welcome blueprint: 1. change the name of some form to mathc new naming standard. 2. adde the preconstructors for the forms that did not have it before

--- AgrimoduleSoftware/ASS/ass/solarvibes/welcome/forms.py
+++ AgrimoduleSoftware/ASS/ass/solarvibes/welcome/forms.py
@@ -3,31 +3,53 @@ from wtforms import StringField, FloatField, SelectField
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length, NumberRange
 
-class PreFarmForm:
+class PreAddFarmForm:
     def __init__(self, farm_name, farm_location, farm_area, farm_cultivation_process):
         self.farm_name                   = farm_name
         self.farm_location               = farm_location
         self.farm_area                   = farm_area
         self.farm_cultivation_process    = farm_cultivation_process
 
-class FarmForm(FlaskForm):
+class AddFarmForm(FlaskForm):
     farm_name                   = StringField('Farm name', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
     farm_location               = StringField('Farm location', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
     farm_area                   = FloatField('Farm Cultivation Area', validators=[DataRequired(), NumberRange(min=1, max=5000, message='Area between 1 and 5000 m2')])
     farm_cultivation_process    = SelectField('Farm Cultivation Process', validators=[DataRequired()], choices=[('Organic','Organic'),('Chemical','Chemical')])
 
+class PreAddCropForm:
+    def __init__(self, field_cultivation_area, field_cultivation_crop, field_cultivation_start_date, field_cultivation_state, field_cultivation_type):
+        # self.field_name                    = field_name
+        self.field_cultivation_area        = field_cultivation_area
+        self.field_cultivation_crop        = field_cultivation_crop
+        self.field_cultivation_start_date  = field_cultivation_start_date
+        self.field_cultivation_state       = field_cultivation_state
+        self.field_cultivation_type        = field_cultivation_type
 
-class FieldForm(FlaskForm):
-    field_name                    = StringField(label='Field name', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
-    field_cultivation_area       = FloatField(label='Field Cultivation Area', validators=[DataRequired(), NumberRange(min=1, max=5000, message='Cultivation area should be maximum as big as your farm')],render_kw={"placeholder":"500.50"})
+class AddCropForm(FlaskForm):
+    # field_name                    = StringField(label='Field name', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
+    field_cultivation_area        = FloatField(label='Field Cultivation Area', validators=[DataRequired(), NumberRange(min=1, max=5000, message='Cultivation area should be maximum as big as your farm')],render_kw={"placeholder":"500.50"})
     field_cultivation_crop        = SelectField(label='Cultivation Crop', validators=[DataRequired()], coerce = int)
     field_cultivation_start_date  = DateField(label='Cultivation Start Date', format='%d %B, %Y', validators=[DataRequired()])
     field_cultivation_state       = SelectField(label='Cultivation State', validators=[DataRequired()], choices=[('new','New'),('already growing','Already Growing')])
     field_cultivation_type        = SelectField(label='Cultivation Type', validators=[DataRequired()], choices=[('mono','Mono'), ('mix','Mix'), ('multi','Multi')])
 
+class PreAddAgrisysForm:
+    def __init__(self, agrimodule_name, agrimodule_identifier):
+        self.agrimodule_name              = agrimodule_name
+        self.agrimodule_identifier        = agrimodule_identifier
+
 class AddAgrisysForm(FlaskForm):
     agrimodule_name              = StringField('Agrimodule name', validators=[DataRequired(), Length(min=2, max=20, message='Give it a name for sanity MAX 30.')])
     agrimodule_identifier        = StringField('Agrimodule code', validators=[DataRequired(), Length(min=2, max=30, message='Your agrimodule system identifier is in the back of your agrimodule.')])
+
+class PreInstallAgrisysForm:
+    def __init__(self, agm_lat, agm_lon, ags_lat, ags_lon, agp_lat, agp_lon):
+        self.agm_lat        = agm_lat
+        self.agm_lon        = agm_lon
+        self.ags_lat        = ags_lat
+        self.ags_lon        = ags_lon
+        self.agp_lat        = agp_lat
+        self.agp_lon        = agp_lon
 
 class InstallAgrisysForm(FlaskForm):
     agm_lat                 = FloatField('Agrimodule latitude location',       validators=[DataRequired(), NumberRange(min=-90, max=90, message='write the lat coordinates')])
