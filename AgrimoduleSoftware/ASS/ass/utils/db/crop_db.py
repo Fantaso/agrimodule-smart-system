71b20607e0a1d1b8f80a4ed3737f791b7a169e52
fantaso <fantaso.code@gmail.com>
2018-08-29 14:58:45 +0200

    CROP INTELIGENCE: 1. created the first dictionary of the datase for small green cardamom details: - crop charactesrictics - soil requirements - weather requirements - growth statges - cropping pattern

--- AgrimoduleSoftware/ASS/ass/utils/db/crop_db.py
+++ AgrimoduleSoftware/ASS/ass/utils/db/crop_db.py
@@ -1,6 +1,6 @@
 # Automatically add crops to the db through SQLAlchemy
 # in run.py directory
-# 				from utils.db.crop_db import * 
+# 				from utils.db.crop_db import *
 
 def add_crops(crops):
 	print('Importing db and Crop')
@@ -15,432 +15,113 @@ def add_crops(crops):
 
 
 crops = {
-			'plum':
-					{
-					'_name':'plum',
-					'_variety':'tomato',
-				 	'_family':'fruit',
-					'_yield':2800, # gr
-					'_space_x':30, # cm
-					'_space_y':30, # cm
-					'_space_z':1.1, # m
-					'_density':0.001111, # plants/cm2
-					'_fruit_quantity':26,
-					'_fruit_size':7.6, # cm
-					'_fruit_weight':62, # gr
-					'_water':1000, # ml/day
-					'_nutrient':18, # mSiemens
-					'_radiation':720, # min/day
-					'_dtg':12, # day
-					'_dtm':180, # day
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.5, 		'_soil_ph_max':6.8,			# pH
-					'_soil_temp_min':15,	'_soil_temp_opt':23,		'_soil_temp_max':30, 		# C
-					'_soil_humi_min':50,	'_soil_humi_opt':60,		'_soil_humi_max':75, 		# %RH
-					'_soil_nutrient_min':2,	'_soil_nutrient_opt':16,	'_soil_nutrient_max':40,	# mSiemens
-					'_air_temp_min':17,		'_air_temp_opt':25,			'_air_temp_max':28, 		# C
-					'_air_humi_min':60,		'_air_humi_opt':63,			'_air_humi_max':70,			# %RH
+	'green cardamom': {
+		'characteristics': {
+			'name':'green cardamom',
+			'variety':'cardamom',
+		 	'family':'dry fruit',
+			'image': '',
+			'yield':122.1153, # gr
+			'canopy_x':30, # cm
+			'canopy_y':30, # cm
+			'canopy_z':300, # cm
+			'density':2500, # plants/m2
+			'fruit_quantity':610.5765,
+			'fruit_size':7.6, # mm
+			'fruit_weight':0.2, # gr
+			'root_depth':45, # cm
+			},
+		'growth_stages': {
+			'in_nursery': {
+				'sowing': 1, # week
+				'germination': 33, # week
+				},
+			'in_field': {
+			 	'transplantation': 1, # week
+				'vegetative_growth': 76, # week
+				'fruit_development': 8, # week
+				'maturity': 8, # week
+				'harvest':4, # week
+				},
+			},
+		'soil_characteristics': {
+			'soil': {
+			 	'preferences': 'loamy forest soils', #
+				},
+			'temperature': {
+			 	'min': 10, # celcius
+				'opt': 18, #
+				'max': 35, #
+				},
+			'moisture': {
+			 	'min': 45, # % moisture
+				'opt': 48, #
+				'max': 50, #
+				},
+			'ph': {
+			 	'min': 4.5, # potential hydrogen - acidicity /alkaline
+				'opt': 6.5, #
+				'max': 7, #
+				},
+			'ec': {
+			 	'min': 0, # electrical conductivity - salinity
+				'opt': 0, #
+				'max': 0, #
+				},
+			},
+		'weather_characteristics': {
+			'weather': {
+			 	'preferences': 'forest weather, humid, normally grown under other bigger crops', #
+				},
+			'temperature': {
+			 	'min': 10, # celcius
+				'opt': 18, #
+				'max': 35, #
+				},
+			'humidity': {
+			 	'min': 60, # % relative humidity
+				'opt': None, #
+				'max': 75, #
+				},
+			'light': {
+			 	'min': 8, # amount of light hours or solar radiation
+				'opt': None, #
+				'max': 12, #
+				},
+			'radiation': {
+			 	'min': None, # watts/m2 or MJoules/day etc amount of light hours or solar radiation
+				'opt': None, #
+				'max': None, #
+				},
+			},
+		'cropping_pattern': {
+			'general': {
+			 	'layout': 'east-west', # directions of plantations
+				'layout_degree': 90, # degrees tor directions of layout
+				},
+			 'seeding': {
+ 			 	'location': 'nursery', #
+ 				'seed_quantity': 40, # gr / bed
+				'bed_characteristics': {
+    			 	'height': 0.2, # m
+    				'width': 1, # m
+   					'length': 6, # m
+    				},
+				'planting_space': {
+    			 	'plant_space': 0., # m
+    				'row_space': 0.1, # m
+    				},
+ 				},
+			'monoculture': {
+			 	'single_row': {
+				 	'plant_distance': 2, # m
+					'row_distance': 2, # m
 					},
-			'romaine':
-					{
-					'_name':'romaine',
-					'_variety':'lettuce',
-				 	'_family':'leafy',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'arugula':
-					{
-					'_name':'arugula',
-					'_variety':'herb',
-				 	'_family':'leafy',
-					'_yield':0.671,
-					'_space_x':2.04,
-					'_space_y':2.04,
-					'_space_z':30,
-					'_density':0.240292,
-					'_fruit_quantity':1,
-					'_fruit_size':25,
-					'_fruit_weight':0.671,
-					'_water':50,
-					'_nutrient':0.5,
-					'_radiation':600,
-					'_dtg':6,
-					'_dtm':47,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.3, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':0,		'_soil_humi_opt':0,		'_soil_humi_max':0,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':18,		'_air_temp_max':26,
-					'_air_humi_min':0,		'_air_humi_opt':0,		'_air_humi_max':0,
-					},
-			'radicchio':
-					{
-					'_name':'radicchio',
-					'_variety':'unknown',
-				 	'_family':'leafy',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'bell pepper':
-					{
-					'_name':'bell pepper',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'cabbage':
-					{
-					'_name':'cabbage',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'coriander':
-					{
-					'_name':'coriander',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'basil':
-					{
-					'_name':'basil',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'squash':
-					{
-					'_name':'squash',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'chive':
-					{
-					'_name':'chive',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'sweet corn':
-					{
-					'_name':'sweet corn',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'black bean':
-					{
-					'_name':'black bean',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'strawberry':
-					{
-					'_name':'strawberry',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'plum2':
-					{
-					'_name':'plum2',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'rice':
-					{
-					'_name':'rice',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'chilli':
-					{
-					'_name':'chilli',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-			'cottom':
-					{
-					'_name':'cottom',
-					'_variety':'something',
-				 	'_family':'something',
-					'_yield':626,
-					'_space_x':20,
-					'_space_y':20,
-					'_space_z':35,
-					'_density':0.0025,
-					'_fruit_quantity':1,
-					'_fruit_size':35,
-					'_fruit_weight':626,
-					'_water':800,
-					'_nutrient':0.8,
-					'_radiation':680,
-					'_dtg':10,
-					'_dtm':70,
-					'_soil_ph_min':6, 		'_soil_ph_opt':6.2, 	'_soil_ph_max':6.8,
-					'_soil_temp_min':0,		'_soil_temp_opt':0,		'_soil_temp_max':0,
-					'_soil_humi_min':40,	'_soil_humi_opt':45,	'_soil_humi_max':60,
-					'_soil_nutrient_min':0,	'_soil_nutrient_opt':0,	'_soil_nutrient_max':0,
-					'_air_temp_min':10,		'_air_temp_opt':16,		'_air_temp_max':19,
-					'_air_humi_min':60,		'_air_humi_opt':65,		'_air_humi_max':70,
-					},
-		}
+				}
+			},
+		},
+	}
+
 
 
 # TO ADD THESE CROP DATASETS RUN:
