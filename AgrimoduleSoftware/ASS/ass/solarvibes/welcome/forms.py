be5c02d4e2d65b3bbbe1ff62ad97d7225de5ae32
Takahiko Okada <takahikookada@gmail.com>
2018-09-03 17:35:08 +0200

    farm_coordinates added and area commented out

--- AgrimoduleSoftware/ASS/ass/solarvibes/welcome/forms.py
+++ AgrimoduleSoftware/ASS/ass/solarvibes/welcome/forms.py
@@ -4,17 +4,18 @@ from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired, Length, NumberRange
 
 class PreAddFarmForm:
-    def __init__(self, farm_name, farm_location, farm_area, farm_cultivation_process):
+    def __init__(self, farm_name, farm_location, farm_coordinates, farm_cultivation_process):
         self.farm_name                   = farm_name
         self.farm_location               = farm_location
-        self.farm_area                   = farm_area
+        self.farm_coordinates            = farm_coordinates
+        # self.farm_area                   = farm_area
         self.farm_cultivation_process    = farm_cultivation_process
 
 class AddFarmForm(FlaskForm):
     farm_name                   = StringField('Farm name', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
     farm_location               = StringField('Farm location', validators=[DataRequired(), Length(min=2, max=30, message='''Your name needs at least 2 characters.''')])
-    farm_coordinates            = HiddenField('')
-    farm_area                   = FloatField('Farm Cultivation Area', validators=[DataRequired(), NumberRange(min=1, max=5000, message='Area between 1 and 5000 m2')])
+    farm_coordinates            = HiddenField('', validators=[DataRequired()])
+    # farm_area                   = FloatField('Farm Cultivation Area', validators=[DataRequired(), NumberRange(min=1, max=5000, message='Area between 1 and 5000 m2')])
     farm_cultivation_process    = SelectField('Farm Cultivation Process', validators=[DataRequired()], choices=[('Organic','Organic'),('Chemical','Chemical')])
 
 class PreAddCropForm:
